0c833ad1fdbd1099e764007df972d971532c2782
Bahm <x.benjamin.ahm@gmail.com>
2021-07-15 14:54:53 +0700

    Added claim takeover feature.

--- src/hasjamon/lecternclaim/LecternClaim.java
+++ src/hasjamon/lecternclaim/LecternClaim.java
@@ -87,7 +87,7 @@ public class LecternClaim extends JavaPlugin{
         pluginManager.registerEvents(new PlayerJoin(this), this);
         pluginManager.registerEvents(new PlayerMove(), this);
         pluginManager.registerEvents(new PlayerQuit(), this);
-        pluginManager.registerEvents(new PlayerDeath(), this);
+        pluginManager.registerEvents(new PlayerDeath(this), this);
         pluginManager.registerEvents(new PlayerRespawn(), this);
         pluginManager.registerEvents(new ChunkLoad(), this);
         if(this.getConfig().getBoolean("disable-freecam-interactions"))
